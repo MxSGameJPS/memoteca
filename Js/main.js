6adea2ea5aed71a684c9ce4de00a43d0dfc20db5
MxSGameJPS <mxsgamejps@gmail.com>
2025-02-11 11:10:22 -0300

    implementação do botão favorito

--- Js/main.js
+++ Js/main.js
@@ -1,37 +1,47 @@
-import ui from "./ui.js";
-import api from "./api.js";
-
+import ui from "./ui.js"
+import api from "./api.js"
 
 document.addEventListener("DOMContentLoaded", () => {
-  ui.renderizarPensamentos();
+  ui.renderizarPensamentos()
 
-  const formularioPensamento = document.getElementById("pensamento-form");
-  const cancelar = document.getElementById("botao-cancelar");
+  const formularioPensamento = document.getElementById("pensamento-form")
+  const botaoCancelar = document.getElementById("botao-cancelar")
+  const inputBusca = document.getElementById("campo-busca")
 
-  
-  formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario);
-  cancelar.addEventListener("click", manipularCancelamento)
-});
+  formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
+  botaoCancelar.addEventListener("click", manipularCancelamento)
+  inputBusca.addEventListener("input", manipularBusca)
+})
 
 async function manipularSubmissaoFormulario(event) {
-  event.preventDefault();
-  const id = document.getElementById("pensamento-id").value;
-  const conteudo = document.getElementById("pensamento-conteudo").value;
-  const autoria = document.getElementById("pensamento-autoria").value;
+  event.preventDefault()
+  const id = document.getElementById("pensamento-id").value
+  const conteudo = document.getElementById("pensamento-conteudo").value
+  const autoria = document.getElementById("pensamento-autoria").value
 
   try {
-    if(id){
-      await api.editarPensamento({id, conteudo, autoria});
+    if (id) {
+      await api.editarPensamento({ id, conteudo, autoria })
     } else {
-      await api.salvarPensamento({conteudo, autoria});
+      await api.salvarPensamento({ conteudo, autoria })
     }
-    ui.renderizarPensamentos();
-  }
-  catch {
-    alert("Erro ao Salvar Pensamento");
+    ui.renderizarPensamentos()
+  } catch {
+    alert("Erro ao salvar pensamento")
   }
 }
 
 function manipularCancelamento() {
-    ui.limparFormulario();
+  ui.limparFormulario()
+}
+
+async function manipularBusca() {
+  const termoBusca = document.getElementById("campo-busca").value
+  try {
+    const pensamentosFiltrados = await api.buscarPensamentosPorTermo(termoBusca)
+    console.log(pensamentosFiltrados)
+    ui.renderizarPensamentos(pensamentosFiltrados)
+  } catch (error) {
+    alert("Erro ao realizar busca")
+  }
 }
--- Js/ui.js
+++ Js/ui.js
@@ -1,66 +1,71 @@
-import api from "./api.js";
+import api from "./api.js"
 
 const ui = {
+
   async preencherFormulario(pensamentoId) {
-    const pensamento = await api.buscarPensamentoPorId(pensamentoId);
-    document.getElementById("pensamento-id").value = pensamento.id;
-    document.getElementById("pensamento-conteudo").value = pensamento.conteudo;
-    document.getElementById("pensamento-autoria").value = pensamento.autoria;
+    const pensamento = await api.buscarPensamentoPorId(pensamentoId)
+    document.getElementById("pensamento-id").value = pensamento.id
+    document.getElementById("pensamento-conteudo").value = pensamento.conteudo
+    document.getElementById("pensamento-autoria").value = pensamento.autoria
   },
 
   limparFormulario() {
-    document.getElementById("pensamento-form").reset();
-    console.log(manipularCancelamento);
+    document.getElementById("pensamento-form").reset()
   },
-  async renderizarPensamentos() {
+
+  async renderizarPensamentos(pensamentosFiltrados = null) {
     const listaPensamentos = document.getElementById("lista-pensamentos")
     const mensagemVazia = document.getElementById("mensagem-vazia")
     listaPensamentos.innerHTML = ""
-
+  
     try {
-      const pensamentos = await api.buscarPensamentos();
-      pensamentos.forEach(ui.adicionarPensamentosNaLista);
+      let pensamentosParaRenderizar
 
-      if (pensamentos.length === 0) {
-        mensagemVazia.style.display = "block";
+      if(pensamentosFiltrados) {
+        pensamentosParaRenderizar = pensamentosFiltrados
       } else {
-        mensagemVazia.style.display = "none";
-        //pensamentos.forEach(ui.adicionarPensamentosLista);
+        pensamentosParaRenderizar = await api.buscarPensamentos()
       }
-    } catch {
-      alert("Erro ao renderizar pensamentos");
+      
+      if (pensamentosParaRenderizar.length === 0) {
+        mensagemVazia.style.display = "block"
+      } else {
+        mensagemVazia.style.display = "none"
+        pensamentosParaRenderizar.forEach(ui.adicionarPensamentoNaLista)
+      } 
     }
-    
+    catch {
+      alert('Erro ao renderizar pensamentos')
+    }
   },
 
-  adicionarPensamentosNaLista(pensamento) {
-    const listaPensamentos = document.getElementById("lista-pensamentos");
-    const li = document.createElement("li");
-    li.setAttribute("data-id", pensamento.id);
-    li.classList.add("li-pensamento");
+  adicionarPensamentoNaLista(pensamento) {
+    const listaPensamentos = document.getElementById("lista-pensamentos")
+    const li = document.createElement("li")
+    li.setAttribute("data-id", pensamento.id)
+    li.classList.add("li-pensamento")
 
-    const iconeAspas = document.createElement("img");
-    iconeAspas.src = "assets/imagens/aspas-azuis.png";
-    iconeAspas.alt = "Aspas azuis";
-    iconeAspas.classList.add("icone-aspas");
+    const iconeAspas = document.createElement("img")
+    iconeAspas.src = "assets/imagens/aspas-azuis.png"
+    iconeAspas.alt = "Aspas azuis"
+    iconeAspas.classList.add("icone-aspas")
 
-    const conteudoPensamento = document.createElement("div");
-    conteudoPensamento.textContent = pensamento.conteudo;
-    conteudoPensamento.classList.add("pensamento-conteudo");
+    const pensamentoConteudo = document.createElement("div")
+    pensamentoConteudo.textContent = pensamento.conteudo
+    pensamentoConteudo.classList.add("pensamento-conteudo")
 
-    const autoriaPensamento = document.createElement("div");
-    autoriaPensamento.textContent = pensamento.autoria;
-    autoriaPensamento.classList.add("pensamento-autoria");
+    const pensamentoAutoria = document.createElement("div")
+    pensamentoAutoria.textContent = pensamento.autoria
+    pensamentoAutoria.classList.add("pensamento-autoria")
 
-    const botaoEditar = document.createElement("button");
-    botaoEditar.classList.add("botao-editar");
-    botaoEditar.onclick = () => ui.preencherFormulario(pensamento.id);
-    
-    const iconeEditar = document.createElement("img");
-    iconeEditar.src = "assets/imagens/icone-editar.png";
-    iconeEditar.alt = "Editar";
-    botaoEditar.appendChild(iconeEditar);
-    
+    const botaoEditar = document.createElement("button")
+    botaoEditar.classList.add("botao-editar")
+    botaoEditar.onclick = () => ui.preencherFormulario(pensamento.id)
+
+    const iconeEditar = document.createElement("img")
+    iconeEditar.src = "assets/imagens/icone-editar.png"
+    iconeEditar.alt = "Editar"
+    botaoEditar.appendChild(iconeEditar)
 
     const botaoExcluir = document.createElement("button")
     botaoExcluir.classList.add("botao-excluir")
@@ -69,7 +74,7 @@ const ui = {
         await api.excluirPensamento(pensamento.id)
         ui.renderizarPensamentos()
       } catch (error) {
-        alert("Erro ao excluir pensamnto")
+        alert("Erro ao excluir pensamento")
       }
     }
 
@@ -78,19 +83,34 @@ const ui = {
     iconeExcluir.alt = "Excluir"
     botaoExcluir.appendChild(iconeExcluir)
 
+    const botaoFavorito = document.createElement("button")
+    botaoFavorito.classList.add("botao-favorito")
+    botaoFavorito.onclick = async () => {
+      try {
+        await api.atualizarFavorito(pensamento.id, !pensamento.favorito)
+        ui.renderizarPensamentos()
+      } catch (error) {
+        alert("Erro ao atualizar favorito")
+      }
+    }
 
-    const icones = document.createElement("div");
-    icones.classList.add("icones");
-    icones.appendChild(botaoEditar);
-    icones.appendChild(botaoExcluir);
-    
+    const iconeFavorito = document.createElement("img")
+    iconeFavorito.src = pensamento.favorito ? "assets/imagens/icone-favorito.png" : "assets/imagens/icone-favorito_outline.png"
+    iconeFavorito.alt = "Favorito"
+    botaoFavorito.appendChild(iconeFavorito)
 
-    li.appendChild(iconeAspas);
-    li.appendChild(conteudoPensamento);
-    li.appendChild(autoriaPensamento);
-    li.appendChild(icones);
-    listaPensamentos.appendChild(li);
-  },
-};
+    const icones = document.createElement("div")
+    icones.classList.add("icones")
+    icones.appendChild(botaoFavorito)
+    icones.appendChild(botaoEditar)
+    icones.appendChild(botaoExcluir)
 
-export default ui;
+    li.appendChild(iconeAspas)
+    li.appendChild(pensamentoConteudo)
+    li.appendChild(pensamentoAutoria)
+    li.appendChild(icones)
+    listaPensamentos.appendChild(li)
+  }
+}
+
+export default ui
